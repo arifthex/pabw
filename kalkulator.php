e3e43164c819acf72e91ce0834003594d4f6c2f7
Arif wicak <ariftheaxr@gmail.com>
2019-10-13 14:46:56 +0700

    add latihan prak

---
+++ kalkulator.php
@@ -0,0 +1,24 @@
+<?php  
+	$a = 15;
+	$b = 5;
+
+	echo "bilangan 1 = $a <br>"; 
+	echo "bilangan 2 = $b <br>";
+	echo "Berikut hasil perhitungan dari setiap operator <br>"; 
+
+	echo "<br> Penjumlahan <br>";
+	$c= $a+$b;
+	echo "hasilnya : $a + $b = $c <br>";
+	echo "<br> Pengurangan <br>";
+	$c=$a-$b;
+	echo "hasilnya : $a - $b = $c <br>";
+	echo "<br> Perkalian <br>";
+	$c=$a*$b;
+	echo "hasilnya : $a * $b = $c <br>";
+	echo "<br> Pembagian <br>";
+	$c=$a/$b;
+	echo "hasilnya : $a / $b = $c <br>";
+	echo "<br> Modulus <br>";
+	$c=$a%$b;
+	echo "hasilnya : $a % $b = $c <br>";
+?>
